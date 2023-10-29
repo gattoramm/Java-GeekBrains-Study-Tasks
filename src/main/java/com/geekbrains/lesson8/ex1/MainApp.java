42f480a406d830ee4fbb21b711a28ab5e9cff870
gattoramm <gattoramm@gmail.com>
2023-10-29 19:04:19 +0300

    примеры по Stream API

---
+++ src/main/java/com/geekbrains/lesson8/ex1/MainApp.java
@@ -0,0 +1,17 @@
+package com.geekbrains.lesson8.ex1;
+
+public class MainApp {
+    public static void main(String[] args) {
+        doSomething(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Java");
+            }
+        });
+
+        doSomething(() -> System.out.println(100));
+    }
+
+    public static void doSomething(Runnable runnable) {
+        runnable.run();
+    }
+}
